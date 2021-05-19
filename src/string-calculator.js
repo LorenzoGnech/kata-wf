e6ecdddfea70bb319cb274b56d9a190b58868319
LorenzoGnech <lorenzognech@gmail.com>
2021-05-19 16:11:45 +0200

    Implemented the basic operations required in step 1

--- src/string-calculator.js
+++ src/string-calculator.js
@@ -1,5 +1,13 @@
-const calculator = (pass, rules) => {
-    // Placeholder
+const calculator = (inputString) => {
+    if(inputString == ''){
+        return 0;
+    }
+    let values = inputString.split(',')
+    if(values.length == 1){
+        return parseInt(values[0])
+    } else{
+        return parseInt(values[0]) + parseInt(values[1])
+    }
 }
 
 module.exports = {
